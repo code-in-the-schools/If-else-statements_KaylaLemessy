297bed96b44a20e3e53276c7a3d0300d41be0dcf
Kayla Lemessy <kaylalemessy15@gmail.com>
2020-07-20 15:28:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+userInput=str(input("What is your name?"))
+inputone=userInput.lower()
+print(inputone)
+
+v=["a","e","i","o","u"]
+for i in inputone:
+  if i in v : 
+    print(i + " is a vowel") 
+  else: 
+    print(i + " is a constant ")
+    
+
+
+
+
+
+
